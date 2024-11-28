8e00dc60bab088a0289486986c879611a690131f
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-11-28 10:59:10 +0000

    Step 0 : Creating New DataBinding Project

---
+++ cwd_databinding/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("dbdemo.cwddatabinding.controller.View1", {
+        onInit() {
+        }
+    });
+});

6d289331f6cf469444306c3239471e718d1728a7
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-12-02 17:00:14 +0000

    Step 15: Aggregation Binding Using a Factory Function

--- cwd_databinding/webapp/controller/Overview.controller.js
+++ cwd_databinding/webapp/controller/Overview.controller.js
@@ -1,21 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "../model/formatter",
-    "../model/formatterValue"
-], (Controller,formatter,formatterValue) => {
+    "sap/ui/core/mvc/Controller", "sap/m/ObjectAttribute", "../model/formatter", "../model/formatterValue"
+], (Controller, ObjectAttribute, formatter, formatterValue) => {
     "use strict";
 
     return Controller.extend("dbdemo.cwddatabinding.controller.Overview", {
-        formatter : formatter,
-        formatterValue : formatterValue,
-        onInit() {
-        },
+        formatter: formatter,
+        formatterValue: formatterValue,
+        onInit() {},
         onItemSelected(oEvent) {
-            var oSelectedItem = oEvent.getSource(); 
+            var oSelectedItem = oEvent.getSource();
             var obindingCtx = oSelectedItem.getBindingContext("products");
             var sPath = obindingCtx.getPath();
             var oProductDetailPanel = this.byId("productDetailsPanel");
-            oProductDetailPanel.bindElement({path : sPath, model : "products"});
+            oProductDetailPanel.bindElement({path: sPath, model: "products"});
+        },
+        productListFactory(sId, oContext) {
+            let oUIControl;
+
+            // Decide based on the data which dependent to clone
+            if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")) { // The item is discontinued, so use a StandardListItem
+                oUIControl = this.byId("productSimple").clone(sId);
+            } else { // The item is available, so we will create an ObjectListItem
+                oUIControl = this.byId("productExtended").clone(sId);
+
+                // The item is temporarily out of stock, so we will add a status
+                if (oContext.getProperty("UnitsInStock") < 1) {
+                    oUIControl.addAttribute(new ObjectAttribute({
+                        text: {
+                            path: "i18n>outOfStock"
+                        }
+                    }));
+                }
+            }
+
+            return oUIControl;
         }
     });
 });
